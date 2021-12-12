42e2077815ddaad1c94ddc046c580f49babc8fba
Maximiliano Greco <mmngreco@gmail.com>
2021-12-12 12:48:49 +0100

    day 2:  part 1 complete

--- 2021/day2/main.go
+++ 2021/day2/main.go
@@ -1,44 +1,128 @@
+/*
+Now, you need to figure out how to pilot this thing.
+
+It seems like the submarine can take a series of commands like forward 1, down
+2, or up 3:
+
+forward X increases the horizontal position by X units.
+down X increases the depth by X units.
+up X decreases the depth by X units.
+
+Note that since you're on a submarine, down and up affect your depth, and so
+they have the opposite result of what you might expect.
+
+The submarine seems to already have a planned course (your puzzle input). You
+should probably figure out where it's going. For example:
+
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+
+Your horizontal position and depth both start at 0. The steps above would then
+modify them as follows:
+
+- forward 5 adds 5 to your horizontal position, a total of 5.
+- down 5 adds 5 to your depth, resulting in a value of 5.
+- forward 8 adds 8 to your horizontal position, a total of 13.
+- up 3 decreases your depth by 3, resulting in a value of 2.
+- down 8 adds 8 to your depth, resulting in a value of 10.
+- forward 2 adds 2 to your horizontal position, a total of 15.
+
+
+After following these instructions, you would have a horizontal position of 15
+and a depth of 10. (Multiplying these together produces 150.)
+
+
+Calculate the horizontal position and depth you would have after following the
+planned course. What do you get if you multiply your final horizontal position
+by your final depth?
+
+*/
+
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "strconv"
-    "strings"
-    // "reflect"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+	// "reflect"
 )
 
 
- type Ship struct {
-     horizontal int
-     vertical int
-     depth int
- }
+var DEBUG bool = false
+
+func readFile(fname string) (nums []string, err error) {
+    // read file
+
+    b, err := ioutil.ReadFile(fname)
+    if err != nil { return nil, err }
+
+    lines := strings.Split(strings.Trim(string(b), "\n"), "\n")
+
+    return  lines, err
+}
 
 
-func str2int(movement string) int {
+
+type Ship struct {
+    horizontal int
+    depth int
+}
+
+
+func (s *Ship) move(movement string) {
     // split and convert movement string into int.
+
     splitted := strings.Fields(movement)
     sdir := splitted[0]
     smod := splitted[1]
     mod, _ := strconv.Atoi(smod)
 
     switch sdir {
-    case "forward":
-        return mod
-    case "down":
-        return mod * -1
-    case "up":
 
+    case "forward":
+        s.move_horizontal(mod)
+    case "down":
+        s.move_depth(mod)
+    case "up":
+        s.move_depth(-mod)
     }
+
 }
 
 func (s *Ship) move_horizontal(x int) {
+    // Move over horizontal
     (*s).horizontal += x
 }
 
+func (s *Ship) move_depth(x int) {
+    // Move over horizontal
+    (*s).depth += x
+}
+
+func (s *Ship) multiply() int {
+    return (*s).depth * (*s).horizontal
+}
 
 
 func main() {
-    fmt.Println()
+
+    ship := Ship{}
+    order_list, _ := readFile("input")
+
+    for idx, order := range order_list {
+
+        if DEBUG { fmt.Println(idx, order) }
+
+        ship.move(order)
+
+        if DEBUG { fmt.Println(ship) }
+    }
+
+    fmt.Println(ship)
+    fmt.Println(ship.multiply())
 }
---
+++ 2021/go.mod
@@ -0,0 +1,3 @@
+module aoc-2021
+
+go 1.16
