09bb9b4435fcb5bbf3bb0106c17f41e5c5091e7e
Maximiliano Greco <mmngreco@gmail.com>
2021-12-14 19:32:45 +0100

    day3: part 1 solved!

--- 2021/day3/main.go
+++ 2021/day3/main.go
@@ -2,17 +2,23 @@
 package main
 
 import (
+    "os"
     "fmt"
     "strings"
     "io/ioutil"
     "strconv"
 )
 
+var DEBUG bool = true
+
+func debug(x interface{} ) {
+    if DEBUG { fmt.Println(x) }
+}
+
 
 func readFile(fname string) (array2d [][]int, err error) {
 
     b, err := ioutil.ReadFile(fname)
-    if err != nil { return nil, err }
 
     lines := strings.Split(strings.Trim(string(b), "\n"), "\n")
 
@@ -63,15 +69,16 @@ func transpose(matrix [][]int) (out [][]int) {
 }
 
 
-func sum(arr [][]int) ([]int) {
-    n := len(arr[0])
-    out := make([]int, n)
-    for idx, row := range arr {
-        for _, elem := range row {
-            out[idx] += elem
-        }
+func sum(matrix [][]int) (out []float32) {
 
+    out = make([]float32, len(matrix))
+
+    for idx, row := range matrix {
+        for _, item := range row {
+            out[idx] += float32(item)
+        }
     }
+
     return out
 }
 
@@ -106,7 +113,24 @@ func gt(array []float32, num float32) (out []int) {
 }
 
 
-func arr2num(arr []int) (int64){
+func lt(array []float32, num float32) (out []int) {
+    n := len(array)
+    out = make([]int, n)
+
+    for idx, item := range array {
+        if item < num {
+            out[idx] = 1
+        } else if item > num {
+            out[idx] = 0
+        }
+    }
+
+    return out
+}
+
+
+
+func arr2num(arr []int) (out int){
     str_list := make([]string, len(arr))
     for i, x := range arr {
         str_list[i] = strconv.Itoa(x)
@@ -114,23 +138,35 @@ func arr2num(arr []int) (int64){
 
     bin := strings.Join(str_list, "")
     x, _ := strconv.ParseInt(bin, 2, 64)
-    return x
+    out = int(x)
+    return out
 }
 
 
-func gamma(arr2d [][]int) (arr []int) {
-    // gamma bits
+func gamma(arr2d [][]int) (out int) {
+    // gamma
     arr_t := transpose(arr2d)
     arr_avg := avg(arr_t)
     arr_gt := gt(arr_avg, 0.5)
-    return arr_gt
+    out = arr2num(arr_gt)
+    return out
 }
 
 
+func epsilon(arr2d [][]int) (out int) {
+    // epsilon
+    arr_t := transpose(arr2d)
+    arr_avg := avg(arr_t)
+    arr_lt := lt(arr_avg, 0.5)
+    out = arr2num(arr_lt)
+    return out
+}
+
+
+
 func main() {
-    file, _ := readFile("input")
-    gamma_arr := gamma(file)
-    num := arr2num(gamma_arr)
-    fmt.Println(gamma_arr)
-    fmt.Println(num)
+    file, _ := readFile(os.Args[1])
+    gamma_rate := gamma(file)
+    epsilon_rate := epsilon(file)
+    fmt.Println(gamma_rate * epsilon_rate)
 }
