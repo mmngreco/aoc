a68f319c12a726b1f6de73e645374606eab58486
Maximiliano Greco <mmngreco@gmail.com>
2021-12-15 07:18:54 +0100

    day3(part2): wip

--- 2021/day3/main.go
+++ 2021/day3/main.go
@@ -16,7 +16,7 @@ func debug(x interface{} ) {
 }
 
 
-func readFile(fname string) (array2d [][]int, err error) {
+func ReadFile(fname string) (array2d [][]int, err error) {
 
     b, err := ioutil.ReadFile(fname)
 
@@ -30,7 +30,7 @@ func readFile(fname string) (array2d [][]int, err error) {
 
     for row_i, l := range lines {
         row := make([]int, ncols)
-        row = row2int(l, row)
+        row = Row2int(l, row)
         out[row_i] = row
     }
 
@@ -38,7 +38,7 @@ func readFile(fname string) (array2d [][]int, err error) {
 }
 
 
-func row2int(l string, row []int) ([]int){
+func Row2int(l string, row []int) ([]int){
 
     for char_i, char := range l {
         // every bit column
@@ -50,7 +50,7 @@ func row2int(l string, row []int) ([]int){
 }
 
 
-func transpose(matrix [][]int) (out [][]int) {
+func Transpose(matrix [][]int) (out [][]int) {
     ncols := len(matrix[0])
     nrows := len(matrix)
 
@@ -69,7 +69,7 @@ func transpose(matrix [][]int) (out [][]int) {
 }
 
 
-func sum(matrix [][]int) (out []float32) {
+func Sum2d(matrix [][]int) (out []float32) {
 
     out = make([]float32, len(matrix))
 
@@ -82,7 +82,7 @@ func sum(matrix [][]int) (out []float32) {
     return out
 }
 
-func avg(matrix [][]int) (out []float32) {
+func Avg2d(matrix [][]int) (out []float32) {
 
     out = make([]float32, len(matrix))
     n32 := 1 / float32(len(matrix[0]))
@@ -97,7 +97,20 @@ func avg(matrix [][]int) (out []float32) {
 }
 
 
-func gt(array []float32, num float32) (out []int) {
+func Avg(array []int) (out float32) {
+    // Calculate the average of an array.
+
+    n32 := 1 / float32(len(array))
+
+    for _, item := range array {
+        out += float32(item) * n32
+    }
+
+    return out
+}
+
+
+func GreaterThan(array []float32, num float32) (out []int) {
     n := len(array)
     out = make([]int, n)
 
@@ -113,7 +126,7 @@ func gt(array []float32, num float32) (out []int) {
 }
 
 
-func lt(array []float32, num float32) (out []int) {
+func LessThan(array []float32, num float32) (out []int) {
     n := len(array)
     out = make([]int, n)
 
@@ -130,43 +143,91 @@ func lt(array []float32, num float32) (out []int) {
 
 
 
-func arr2num(arr []int) (out int){
+func BitArrayToInt(arr []int) (out int) {
+    // Convert bitArray to int
+
+    // Convert int array to str
     str_list := make([]string, len(arr))
     for i, x := range arr {
         str_list[i] = strconv.Itoa(x)
     }
+    binary_str := strings.Join(str_list, "")
 
-    bin := strings.Join(str_list, "")
-    x, _ := strconv.ParseInt(bin, 2, 64)
-    out = int(x)
+    // Convert binary str to decimal
+    decimal, _ := strconv.ParseInt(binary_str, 2, 64)
+    out = int(decimal)
+
     return out
 }
 
 
-func gamma(arr2d [][]int) (out int) {
-    // gamma
-    arr_t := transpose(arr2d)
-    arr_avg := avg(arr_t)
-    arr_gt := gt(arr_avg, 0.5)
-    out = arr2num(arr_gt)
+func Gamma(arr2d [][]int) (out int) {
+    // oxygen generator rating
+
+    // get most common bit
+
+    bitColumn := 0
+    bit := MostCommonBit(file[bitColumn])
+    // filter file where first bit is equal to the most common bit
+    file := Filter2d(file, bit, bitColumn)
+    // repeat with the next bit column until only ramains one entry.
     return out
 }
 
 
-func epsilon(arr2d [][]int) (out int) {
-    // epsilon
-    arr_t := transpose(arr2d)
-    arr_avg := avg(arr_t)
-    arr_lt := lt(arr_avg, 0.5)
-    out = arr2num(arr_lt)
+func MostCommonBit(bitColumn []int) int {
+    // oxygen generator rating
+    avg := Avg(bitColumn)
+    if avg > 0.5 {
+        return 1
+    } else {
+        return 0
+    }
+}
+
+func Epsilon(arr2d [][]int) (out int) {
+    // CO2 scrubber rating
+    arr_t := Transpose(arr2d)
+    arr_avg := Avg2d(arr_t)
+    arr_lt := LessThan(arr_avg, 0.5)
+    out = BitArrayToInt(arr_lt)
     return out
 }
+
+
+func Filter2d(matrix [][]int, element int, position int) (out [][]int) {
+    matrix = Transpose(matrix)
+    matrix_column := matrix[position]
+
+    // find row positions
+    var row_positions []int
+    for position, value := range matrix_column {
+        if value == element {
+            row_positions = append(row_positions, position)
+        }
+    }
+
+
+    // select positions
+    for _, pos := range row_positions {
+        out = append(out, matrix[pos])
+    }
+    return out
+}
+
 
 
 
 func main() {
-    file, _ := readFile(os.Args[1])
-    gamma_rate := gamma(file)
-    epsilon_rate := epsilon(file)
+    file, _ := ReadFile(os.Args[1])
+    // Read the file
+    // get most common bit
+    bitColumn := 0
+    bit := MostCommonBit(file[bitColumn])
+    // filter file where first bit is equal to the most common bit
+    file := Filter2d(file, bit, bitColumn)
+    // repeat with the next bit column until only ramains one entry.
+    gamma_rate := Gamma(file)
+    epsilon_rate := Epsilon(file)
     fmt.Println(gamma_rate * epsilon_rate)
 }
