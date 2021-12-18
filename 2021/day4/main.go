356e60fc449699e27420f087c06d8edc3b8626db
Maximiliano Greco <mmngreco@gmail.com>
2021-12-18 12:24:45 +0100

    day4: wip

--- 2021/day4/main.go
+++ 2021/day4/main.go
@@ -10,24 +10,40 @@ import (
 
 
 func main() {
+
     // read file from arguments
     input := os.Args[1]
     b, _ := ioutil.ReadFile(input)
     file := string(b)
-    out := strings.Split(file, "\n\n")
+    game_list := strings.Split(file, "\n\n")
 
-    // exlore content
-    for i, v := range out {
-        if i == 0 {
-            // NUMBERS
-            ans := strings.Split(v, ",")
-            fmt.Println(ans)
-        } else {
-            // BOARDS
-            fmt.Println(v)
-            // Process de board
-            // Convert them into a matrix ??
-        }
-        fmt.Println("")
+    // NUMBERS
+    // In the first row there are the randomn numbers.
+    ans := strings.Split(strings.Trim(game_list[0], "\n"), ",")
+    fmt.Println(ans)
+
+    // BOARDS
+    // below are the 5x5 boards
+    board_list_str := game_list[1]
+    // fmt.Println(v)
+    for strings.Contains(board_list_str, "  ") {
+        board_list_str = strings.ReplaceAll(board_list_str, "  ", " ")
     }
+    board_list_str = strings.Trim(board_list_str, "\n")
+    board_list := strings.Split(board_list_str, "\n ")
+
+    for _, a := range ans {
+        for i, row := range board_list {
+            fmt.Println("")
+            fmt.Println("board n", i)
+            fmt.Println("row", i, "value", row)
+            for j, element := range strings.Split(row, " ") {
+                fmt.Println("column, element, number:", j, element, a)
+            }
+        }
+    }
+
+    // Process de board
+    // Convert them into a matrix ??
+    fmt.Println("")
 }
