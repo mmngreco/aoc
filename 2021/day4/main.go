c5fefce1124da35d33f5f0378ef99e3de31dc769
Maximiliano Greco <mmngreco@gmail.com>
2021-12-26 01:49:10 +0100

    day4: completed!!! (⌐■_■)

--- 2021/day4/main.go
+++ 2021/day4/main.go
@@ -1,53 +1,3 @@
-/* Day 4: Bingo!
-
-
-Part 1
-
-7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
-
-22 13 17 11  0
- 8  2 23  4 24
-21  9 14 16  7
- 6 10  3 18  5
- 1 12 20 15 19
-
- 3 15  0  2 22
- 9 18 13 17  5
-19  8  7 25 23
-20 11 10 24  4
-14 21 16 12  6
-
-14 21 17 24  4
-10 16 15  9 19
-18  8 23 26 20
-22 11 13  6  5
- 2  0 12  3  7
-
-
-The score of the winning board can now be calculated.
-
-1. Start by finding the sum of all unmarked numbers on that board; in this
-   case, the sum is 188.
-2. Then, multiply that sum by the number that was just called when the board
-   won, 24, to get the final score, 188 * 24 = 4512.
-
-
-Part 2
-
-
-the safe thing to do is to figure out which board will win last and choose that
-one. That way, no matter which boards it picks, it will win for sure.
-
-In the above example, the second board is the last to win, which happens after
-13 is eventually called and its middle column is completely marked. If you were
-to keep playing until this point, the second board would have a sum of unmarked
-numbers equal to 148 for a final score of 148 * 13 = 1924.
-
-Figure out which board will win last. Once it wins, what would its final score
-be?
-
-
-*/
 package main
 
 import (
@@ -55,120 +5,215 @@ import (
 	"io/ioutil"
 	"os"
 	"strconv"
-
-	// "strconv"
 	"strings"
 )
 
-var DELTA int = 1
+const (
+    Unmarked uint = iota
+    Marked   uint = iota
+)
 
-func Sum2d(arr [5][5]int) int {
-    var out int = 0
-    for _, row := range arr {
-        for _, ele := range row {
-            if ele == 0 { continue }
-            // HACK reduce by one
-            out += ele - DELTA
+
+type Board struct {
+    board [5][5]uint
+    marked [5][5]uint
+    lastNumber uint
+    iterations uint
+}
+
+
+func (b *Board) checkHorizontal() bool {
+    for r := 0; r < 5; r++ {
+        if (*&b.marked[r][0] == Marked) &&
+           (*&b.marked[r][1] == Marked) &&
+           (*&b.marked[r][2] == Marked) &&
+           (*&b.marked[r][3] == Marked) &&
+           (*&b.marked[r][4] == Marked) {
+               return true
         }
     }
+    return false
+}
+
+
+func (b *Board) checkVertical() bool {
+    for c := 0; c < 5; c++ {
+        if (*&b.marked[0][c] == Marked) &&
+           (*&b.marked[1][c] == Marked) &&
+           (*&b.marked[2][c] == Marked) &&
+           (*&b.marked[3][c] == Marked) &&
+           (*&b.marked[4][c] == Marked) {
+               return true
+        }
+    }
+    return false
+}
+
+
+func (b *Board) markNumber(num uint) {
+    switch num {
+        case *&b.board[0][0]: *&b.marked[0][0] = Marked
+        case *&b.board[0][1]: *&b.marked[0][1] = Marked
+        case *&b.board[0][2]: *&b.marked[0][2] = Marked
+        case *&b.board[0][3]: *&b.marked[0][3] = Marked
+        case *&b.board[0][4]: *&b.marked[0][4] = Marked
+        case *&b.board[1][0]: *&b.marked[1][0] = Marked
+        case *&b.board[1][1]: *&b.marked[1][1] = Marked
+        case *&b.board[1][2]: *&b.marked[1][2] = Marked
+        case *&b.board[1][3]: *&b.marked[1][3] = Marked
+        case *&b.board[1][4]: *&b.marked[1][4] = Marked
+        case *&b.board[2][0]: *&b.marked[2][0] = Marked
+        case *&b.board[2][1]: *&b.marked[2][1] = Marked
+        case *&b.board[2][2]: *&b.marked[2][2] = Marked
+        case *&b.board[2][3]: *&b.marked[2][3] = Marked
+        case *&b.board[2][4]: *&b.marked[2][4] = Marked
+        case *&b.board[3][0]: *&b.marked[3][0] = Marked
+        case *&b.board[3][1]: *&b.marked[3][1] = Marked
+        case *&b.board[3][2]: *&b.marked[3][2] = Marked
+        case *&b.board[3][3]: *&b.marked[3][3] = Marked
+        case *&b.board[3][4]: *&b.marked[3][4] = Marked
+        case *&b.board[4][0]: *&b.marked[4][0] = Marked
+        case *&b.board[4][1]: *&b.marked[4][1] = Marked
+        case *&b.board[4][2]: *&b.marked[4][2] = Marked
+        case *&b.board[4][3]: *&b.marked[4][3] = Marked
+        case *&b.board[4][4]: *&b.marked[4][4] = Marked
+    }
+}
+
+func (b *Board) Sum() uint {
+    var out uint = 0
+    if *&b.marked[0][0] == Unmarked { out += *&b.board[0][0] }
+    if *&b.marked[0][1] == Unmarked { out += *&b.board[0][1] }
+    if *&b.marked[0][2] == Unmarked { out += *&b.board[0][2] }
+    if *&b.marked[0][3] == Unmarked { out += *&b.board[0][3] }
+    if *&b.marked[0][4] == Unmarked { out += *&b.board[0][4] }
+    if *&b.marked[1][0] == Unmarked { out += *&b.board[1][0] }
+    if *&b.marked[1][1] == Unmarked { out += *&b.board[1][1] }
+    if *&b.marked[1][2] == Unmarked { out += *&b.board[1][2] }
+    if *&b.marked[1][3] == Unmarked { out += *&b.board[1][3] }
+    if *&b.marked[1][4] == Unmarked { out += *&b.board[1][4] }
+    if *&b.marked[2][0] == Unmarked { out += *&b.board[2][0] }
+    if *&b.marked[2][1] == Unmarked { out += *&b.board[2][1] }
+    if *&b.marked[2][2] == Unmarked { out += *&b.board[2][2] }
+    if *&b.marked[2][3] == Unmarked { out += *&b.board[2][3] }
+    if *&b.marked[2][4] == Unmarked { out += *&b.board[2][4] }
+    if *&b.marked[3][0] == Unmarked { out += *&b.board[3][0] }
+    if *&b.marked[3][1] == Unmarked { out += *&b.board[3][1] }
+    if *&b.marked[3][2] == Unmarked { out += *&b.board[3][2] }
+    if *&b.marked[3][3] == Unmarked { out += *&b.board[3][3] }
+    if *&b.marked[3][4] == Unmarked { out += *&b.board[3][4] }
+    if *&b.marked[4][0] == Unmarked { out += *&b.board[4][0] }
+    if *&b.marked[4][1] == Unmarked { out += *&b.board[4][1] }
+    if *&b.marked[4][2] == Unmarked { out += *&b.board[4][2] }
+    if *&b.marked[4][3] == Unmarked { out += *&b.board[4][3] }
+    if *&b.marked[4][4] == Unmarked { out += *&b.board[4][4] }
     return out
 }
 
 
+func (b *Board) Points() uint {
+    return (*b).Sum() * (*b).lastNumber
+}
 
-func drawNumbers(file string) []int {
+
+func (b *Board) print() {
+    var i uint
+    fmt.Println("Board")
+    fmt.Println("-----")
+    for i = 0; i < 5; i++ {
+        fmt.Println(
+            "\t",
+            (*b).board[i][0],
+            "\t",
+            (*b).board[i][1],
+            "\t",
+            (*b).board[i][2],
+            "\t",
+            (*b).board[i][3],
+            "\t",
+            (*b).board[i][4],
+            "\t",
+            "",
+            "\t",
+            (*b).marked[i][0],
+            "\t",
+            (*b).marked[i][1],
+            "\t",
+            (*b).marked[i][2],
+            "\t",
+            (*b).marked[i][3],
+            "\t",
+            (*b).marked[i][4],
+        )
+    }
+    fmt.Println("last:", (*b).lastNumber)
+    fmt.Println("sum:", (*b).Sum())
+}
+
+func (b *Board) done() bool {
+    return (*b).checkHorizontal() || (*b).checkVertical()
+}
+
+
+func (b *Board) markAll(numArr []uint) uint {
+    var num uint
+
+    for i := 0; i < len(numArr); i++ {
+
+        num = numArr[i]
+        (*b).markNumber(num)
+
+        if (*b).done() {
+            (*b).lastNumber = num
+            (*b).iterations = uint(i)
+            return num * (*b).Sum()
+        }
+    }
+    return 0
+}
+
+
+func drawNumbers(file string) []uint {
+
     // array of numbers to check in each board
     guest_arr := strings.Split(strings.Split(file, "\n\n")[0], ",")
-    numbers := make([]int, len(guest_arr))
+    numbers := make([]uint, len(guest_arr))
+
     for i, num := range guest_arr {
         v, _ := strconv.Atoi(num)
-        numbers[i] = v + DELTA
+        numbers[i] = uint(v)
     }
+
     return numbers
 }
 
 
-func setBoards(file string) [][5][5]int {
-    board_str_list := strings.Split(file, "\n\n")[1:]
-    board_list := make([][5][5]int, len(board_str_list))
-    for ib, board := range board_str_list {
-        board := strings.Split(board, "\n")
-        var board_matrix [5][5]int
+func buildBoards(file string) []Board {
+
+    boardRaw := strings.Split(file, "\n\n")[1:]
+    board_list := []Board{}
+
+    for _, board := range boardRaw {
+
+        // Convert string to matrix of integers
+        boardArr := strings.Split(board, "\n")
+        var board_matrix [5][5]uint
         var col int
-        for row, row_str := range board {
+        for row, row_str := range boardArr {
             col = 0
             for _, e := range strings.Split(row_str, " "){
                 if len(e) == 0 { continue }
                 v, _ := strconv.Atoi(e)
-                board_matrix[row][col] = v + DELTA
+                board_matrix[row][col] = uint(v)
                 col++
             }
         }
-        board_list[ib] = board_matrix
+
+        // Create a board
+        board_list = append(board_list, Board{board: board_matrix})
     }
     return board_list
 }
-
-
-func bestBoardChecker(file string) int {
-
-    // Build array of choosing numbers
-    numbers := drawNumbers(file)
-    board_list := setBoards(file)
-
-    // check numbers
-    nboard := len(board_list)
-    rows_counter := make([][5][]int, nboard)
-    cols_counter := make([][5][]int, nboard)
-
-    num := 0
-    value := 0
-    done := false
-    points := 0
-    MAX_LEN := 5
-    X := 0
-
-    for inum := 0; (inum < len(numbers)) && !done; inum++ {
-        num = numbers[inum]
-
-        for iboard := 0; (iboard < nboard) && !done; iboard++ {
-            board := &board_list[iboard]
-
-            for irow := 0; (irow < MAX_LEN) && !done; irow++{
-                row := &board[irow]
-
-                for icol := 0; (icol < MAX_LEN) && !done; icol++ {
-                    value = (*row)[icol]
-                    row_current := &rows_counter[iboard][irow]
-                    col_current := &cols_counter[iboard][icol]
-
-                    if num == value {
-                        // follow the numbers
-                        *row_current = append((*row_current), num-DELTA)
-                        *col_current = append((*col_current), num-DELTA)
-                        // mark the board
-                        (*row)[icol] = X
-                    }
-
-                    if len(*row_current) == MAX_LEN {
-                        done = true
-                        last := (*row_current)[4]
-                        points = Sum2d(*board) * last
-                    }
-                    if len(*col_current) == MAX_LEN {
-                        done = true
-                        last := (*col_current)[4]
-                        points = Sum2d(*board) * last
-                    }
-                }
-            }
-        }
-    }
-
-    return points
-}
-
-
 
 
 // Read files from arg
@@ -184,7 +229,20 @@ func readFile() string {
 func main() {
 
     file := readFile()
-    points := bestBoardChecker(file)
-    fmt.Println(points)
+    numbers := drawNumbers(file)
+    board_list := buildBoards(file)
 
+    var farIter uint = 0
+    var points uint = 0
+
+    for i := 0; i < len(board_list); i++ {
+
+        board_list[i].markAll(numbers)
+
+        if farIter < board_list[i].iterations {
+            farIter = board_list[i].iterations
+            points = board_list[i].Points()
+        }
+    }
+    fmt.Println(farIter, points)
 }
