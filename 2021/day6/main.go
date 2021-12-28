b45398e5f8a99e11eca1d69589625693493177a5
Maximiliano Greco <mmngreco@gmail.com>
2021-12-28 17:38:27 +0100

    day6: completed! (⌐■_■)

--- 2021/day6/main.go
+++ 2021/day6/main.go
@@ -1,5 +1,16 @@
 /*
 
+--- Part One ---
+So, suppose you have a lanternfish with an internal timer value of 3:
+
+- After one day, its internal timer would become 2.
+- After another day, its internal timer would become 1.
+- After another day, its internal timer would become 0.
+- After another day, its internal timer would reset to 6, and it would create a
+  new lanternfish with an internal timer of 8.
+- After another day, the first lanternfish would have an internal timer of 5,
+  and the second lanternfish would have an internal timer of 7.
+
 Initial state: 3,4,3,1,2
 After  1 day:  2,3,2,0,1
 After  2 days: 1,2,1,6,0,8
@@ -20,6 +31,12 @@ After 16 days: 1,2,1,6,0,1,2,3,3,4,1,2,3,3,3,4,4,5,5,6,8
 After 17 days: 0,1,0,5,6,0,1,2,2,3,0,1,2,2,2,3,3,4,4,5,7,8
 After 18 days: 6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8
 
+--- Part Two ---
+Suppose the lanternfish live forever and have unlimited food and space. Would they take over the entire ocean?
+
+After 256 days in the example above, there would be a total of 26984457539 lanternfish!
+
+How many lanternfish would there be after 256 days?
 */
 package main
 
@@ -32,14 +49,14 @@ import (
 )
 
 
-func readFile() (arr []uint8) {
+func readFile() (arr []uint64) {
     file := os.Args[1]
     b, _ := ioutil.ReadFile(file)
     txt := strings.Trim(string(b), "\n")
 
     for _, v := range strings.Split(txt, ",") {
         val, _ := strconv.Atoi(v)
-        arr = append(arr, uint8(val))
+        arr = append(arr, uint64(val))
     }
 
     return arr
@@ -48,33 +65,56 @@ func readFile() (arr []uint8) {
 
 
 const (
-    NewFish uint8 = 8
-    Reset uint8 = 6
+    NewFish uint64 = 8
+    Reset uint64 = 6
 )
 
 
-func sim() []uint8 {
+func sim() []uint64 {
 
 
     maxDays, _ := strconv.Atoi(os.Args[2])
     arr := readFile()
-    // fmt.Println(arr)
 
-    for d := 0; d < maxDays; d++ {
-        // iteration: day i
-        n := len(arr)
-        for i := 0; i < n; i++ {
-            if arr[i] == 0 {
-                arr[i] = Reset
-                arr = append(arr, NewFish)
-            } else {
-                arr[i]--
-            }
-        }
-        // fmt.Println(arr)
+    lifeCycle := [9]uint64{}
+    for _, life := range arr {
+        if life == 0 { lifeCycle[0]++ }
+        if life == 1 { lifeCycle[1]++ }
+        if life == 2 { lifeCycle[2]++ }
+        if life == 3 { lifeCycle[3]++ }
+        if life == 4 { lifeCycle[4]++ }
+        if life == 5 { lifeCycle[5]++ }
+        if life == 6 { lifeCycle[6]++ }
     }
-    fmt.Println(len(arr))
-    return arr
+
+    for d := 1; d <= maxDays; d++ {
+        for i, n := range lifeCycle {
+            // i = 0, n = 0
+            if i == 1 { lifeCycle[1] -= n; lifeCycle[0] += n }
+            if i == 2 { lifeCycle[2] -= n; lifeCycle[1] += n }
+            if i == 3 { lifeCycle[3] -= n; lifeCycle[2] += n }
+            if i == 4 { lifeCycle[4] -= n; lifeCycle[3] += n }
+            if i == 5 { lifeCycle[5] -= n; lifeCycle[4] += n }
+            if i == 6 { lifeCycle[6] -= n; lifeCycle[5] += n }
+            if i == 7 { lifeCycle[7] -= n; lifeCycle[6] += n }
+            if i == 8 { lifeCycle[8] -= n; lifeCycle[7] += n }
+            if i == 0 { lifeCycle[0] -= n; lifeCycle[6] += n  ; lifeCycle[8] += n }
+        }
+        // fmt.Println(lifeCycle)
+        // fmt.Println(Sum(lifeCycle))
+
+    }
+    // fmt.Println(lifeCycle)
+    fmt.Println(Sum(lifeCycle))
+    return lifeCycle[:]
+}
+
+
+func Sum(arr [9]uint64) (out uint64) {
+    for _, v := range arr {
+        out += v
+    }
+    return out
 }
 
 
